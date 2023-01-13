488a0a65fa7a3d4e4fcd67b34b80fa51f033b022
shashank <shashankpandey056@gmail.com>
2023-01-13 21:35:19 +0530

    chnages for custom promise

--- Week-4/4.1.js
+++ Week-4/4.1.js
@@ -1,11 +1,46 @@
 const getNumber =()=>{
-    return new Promise ((resolve,reject)=>{
-    const randNumber = parseInt(Math.random()*100,10)
-    setTimeout(()=>{
-        if(randNumber%5 ===0) reject(`a promise is rejected ${randNumber}`)
-        else resolve(`promise is resolved :${randNumber}`)
-    },randNumber*100)
-});
+   const randNumber = parseInt(Math.random()*100,10)
+   return randNumber
+
 }
-const p = getNumber()
-p.then((x)=>console.log(x)).catch((err)=>console.log(err))
+
+let num = getNumber()
+function isDivisible(num){
+    return  new myPromise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(num%5 ===0){
+                resolve(`${num} is divisible by 5`)
+            }
+            else{
+                reject(`${num} is not divisble by 5`)
+            }
+        },2000)
+    })
+}
+function myPromise(executor){
+    let onReject;
+    let onResolve;
+    this.then=(fn)=>{
+        onResolve = fn
+        return this
+    }
+    this.catch=(fn)=>{
+        onReject = fn
+        return this
+    }
+    function resolve(val){
+        onResolve(val)
+    }
+    function reject(val){
+        onReject(val)
+    }
+    executor(resolve,reject)
+
+
+}
+isDivisible(num).then((x)=>{
+    console.log(x)
+})
+.catch((err)=>{
+    console.log(err)
+})
