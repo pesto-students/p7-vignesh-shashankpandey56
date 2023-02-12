c73b780501c3e81f8d5607eaa093d039d5bed1ac
shashank <shashankpandey056@gmail.com>
2023-02-12 21:51:33 +0530

    created  to do app

---
+++ Week-12/todo-app/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import {useState} from "react"
+
+function App() {
+  const [todos,setTodos] = useState([{text:"new to do app",completed:false},
+  {text:"learn react",completed:false},
+  {text:"learn node js",completed:false}])
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].completed = true;
+    setTodos(newTodos);
+  };
+ 
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo,index) => (
+          <div
+            className="todo"
+            style={{ textDecoration: todo.completed ? "line-through" : "" }}
+            key={index}
+            onClick={() => completeTodo(index)}
+          >
+            {todo.text}
+          </div>
+        ))}
+        <div className="add-todo">
+          <form onSubmit={e => {
+            e.preventDefault();
+            addTodo(e.target.elements.todo.value);
+            e.target.elements.todo.value = "";
+          }}>
+            <input type="text" name="todo" />
+            <button type="submit">Add Todo</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
